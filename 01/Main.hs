7a3dd76d2b0bcfdcdeed5e51fac001a6f4b044ae
b1 2021 <b1@localhost>
2021-02-28 12:32:55 +0000

    s1 x3

--- 01/Main.hs
+++ 01/Main.hs
@@ -3,8 +3,4 @@ module Main where
 import System.Environment
 
 main :: IO ()
-main = do
-  args <- getArgs
-  let nums :: [Int]
-      nums = map read args
-  putStrLn . show $ foldl (+) 0 nums
+main = getLine >>= putStrLn
