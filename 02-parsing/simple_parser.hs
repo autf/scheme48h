3f36f4c7c59b091967b343f222815022a404f58a
b1 2021 <b1@localhost>
2021-03-01 01:28:34 +0000

    s2 $ parse (String <|> Atom <|> Number)

--- 02-parsing/simple_parser.hs
+++ 02-parsing/simple_parser.hs
@@ -7,15 +7,46 @@ symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 spaces :: Parser ()
 spaces = skipMany1 space
 
--- spaces :: Parser String
--- spaces = many1 space
+-- readExp :: String -> String
+-- readExp input = case parse (spaces >> symbol) "lisp" input of
+--   Left err -> "<failed>: " ++ show err
+--   Right val -> "accepted"
 
-readExp :: String -> String
-readExp input = case parse (spaces >> symbol) "lisp" input of
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | String String
+             | Number Integer
+             | Bool Bool
+
+parseString :: Parser LispVal
+parseString = do
+  char '"'
+  str <- many $ noneOf "\""
+  char '"'
+  return $ String str
+
+parseAtom :: Parser LispVal
+parseAtom = do
+  c <- letter <|> symbol
+  cs <- many (letter <|> symbol <|> digit)
+  return $ case c:cs of
+    "#t" -> Bool True
+    "#f" -> Bool False
+    atom -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = Number . read <$> many1 digit
+
+parseExpr :: Parser LispVal
+parseExpr = parseString <|> parseAtom <|> parseNumber
+
+readExpr :: String -> String
+readExpr input = case parse parseExpr "lisp" input of
   Left err -> "<failed>: " ++ show err
-  Right val -> "accepted"
+  Right _ -> "accepted!"
 
 main :: IO ()
 main = do
   (arg:_) <- getArgs
-  putStrLn . readExp $ arg
+  putStrLn . readExpr $ arg
