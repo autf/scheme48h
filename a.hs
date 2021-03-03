7263670ae2f03f6e5c5f1a0eab58b37e09f65494
b1 2021 <b1@localhost>
2021-03-03 11:44:12 +0000

    s4: half way

--- a.hs
+++ a.hs
@@ -1,5 +1,6 @@
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
+import Control.Monad.Except
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
@@ -58,10 +59,10 @@ parseQuoted = do
   e <- parseExpr
   return $ List [Atom "quote", e]
 
-readExpr :: String -> LispVal
+readExpr :: String -> ThrowsError LispVal
 readExpr input = case parse parseExpr "lisp" input of
-  Left err -> String $ "<failed>: " ++ show err
-  Right val -> val
+  Left err -> throwError $ Parser err
+  Right val -> return val
 
 showVal :: LispVal -> String
 showVal (String cs) = "\"" ++ cs ++ "\""
@@ -77,19 +78,21 @@ unwordList = unwords . map showVal
 
 instance Show LispVal where show = showVal
 
-eval :: LispVal -> LispVal
-eval v@(String _) = v
-eval v@(Number _) = v
+eval :: LispVal -> ThrowsError LispVal
+eval v@(String _) = return v
+eval v@(Number _) = return v
 --eval v@(Atom _) = v
-eval v@(Bool _) = v
-eval (List [Atom "quote", xs]) = xs
-eval (List (Atom fn : xs)) = apply fn $ map eval xs
+eval v@(Bool _) = return v
+eval (List [Atom "quote", xs]) = return xs
+eval (List (Atom fn : xs)) = apply fn =<< mapM eval xs
+eval badForm = throwError $ BadSpecialForm "Unrecognized special form" badForm
 
-apply :: String -> [LispVal] -> LispVal
-apply fn xs = maybe (Bool False) ($ xs) $
-  lookup fn primitives
+apply :: String -> [LispVal] -> ThrowsError LispVal
+apply fn xs = maybe (throwError $ NotFunction "Unrecognized primitive function args" fn)
+              ($ xs)
+              (lookup fn primitives)
 
-primitives :: [(String, [LispVal] -> LispVal)]
+primitives :: [(String, [LispVal] -> ThrowsError LispVal)]
 primitives = [("+", numericBinop (+)),
               ("-", numericBinop (-)),
               ("*", numericBinop (*)),
@@ -98,17 +101,48 @@ primitives = [("+", numericBinop (+)),
               ("quotient", numericBinop quot),
               ("remainder", numericBinop rem)]
 
-numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> LispVal
-numericBinop op vs = Number $ foldl1 op $ map unpackNum vs
+numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> ThrowsError LispVal
+numericBinop _ [] = throwError $ NumArgs 2 []
+numericBinop _ x@[_] = throwError $ NumArgs 2 x
+numericBinop op vs = return . Number . foldl1 op =<< mapM unpackNum vs
 
-unpackNum :: LispVal -> Integer
-unpackNum (Number n) = n
+unpackNum :: LispVal -> ThrowsError Integer
+unpackNum (Number n) = return n
 unpackNum (String s) = let parsed = reads s :: [(Integer, String)] in
   if null parsed
-  then 0
-  else fst $ parsed !! 0
+  then throwError $ TypeMismatch "number" $ String s
+  else return . fst $ parsed !! 0
 --unpackNum (List [n]) = unpackNum n
-unpackNum _ = 0
+unpackNum nan = throwError $ TypeMismatch "number" nan
+
+data LispError = NumArgs Integer [LispVal]
+               | TypeMismatch String LispVal
+               | Parser ParseError
+               | BadSpecialForm String LispVal
+               | NotFunction String String
+               | UnboundVariable String String
+               | Default String
+
+showError :: LispError -> String
+showError (UnboundVariable msg varname) = msg ++ ": " ++ varname
+showError (NotFunction msg fn) = msg ++ ": " ++ fn
+showError (BadSpecialForm msg form) = msg ++ ": " ++ show form
+showError (NumArgs expected found) = "Expected " ++ show expected ++ " args; found values " ++
+                                     unwordList found
+showError (TypeMismatch expected found) = "Invalid type: expected " ++ expected ++ ", found " ++ show found
+showError (Parser err) = "Parser error at " ++ show err
+
+instance Show LispError where show = showError
+
+type ThrowsError = Either LispError
+
+trapError action = catchError action (return . show)
+
+extractValue :: ThrowsError a -> a
+extractValue (Right v) = v
 
 main :: IO ()
-main = print . eval . readExpr . head =<< getArgs
+main = do
+  args <- getArgs
+  putStrLn $ extractValue . trapError $ return . show <$> eval =<< (readExpr $ head args)
+--main = print =<< eval =<< readExpr . head =<< getArgs
